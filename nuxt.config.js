7c9191e6a381abe4d0e1443bac202e5fc1bcfb37
Jesse Rowley <rowleyj@mcmaster.ca>
2021-09-29 22:07:18 -0400

    color logos

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,13 +68,16 @@ export default {
     theme: {
       // dark: true,
       themes: {
-          primary: colors.cyan.lighten4,
+        light:{
+          primary: colors.orange.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
+        }
+          
       }
     }
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,56 @@
 <template>
 <v-container>
-  <v-row justify="center" align="center">
-    <v-img src="/hooli.png" max-height="200" max-width="200"></v-img>
-  </v-row>
-  <v-row justify="center" align="center">
-    <h2>
-    Cycling made safe
-    </h2>
+  <v-row justify="center" align="center" >
+    <v-card flat>
+      <v-img src="/hooli.jpg" max-height="300" max-width="300" style="border-radius: 50%"></v-img>
+      <v-container>
+        <v-row justify="center">
+          <v-card-title>
+            Cycling made safe
+          </v-card-title>
+        </v-row>
+      </v-container>
+      
+    </v-card>
+    
   </v-row>
   <v-row justify="center" align="center">
     <v-col md="8">
-      <v-card>
-        blah blah
+      <v-card shaped>
+        <v-card-text>
+          {{fakeText}}
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
   <v-row justify="center" align="center">
     <v-col md="8">
-      <v-card>
-        blah blah
+      <v-card shaped>
+        <v-card-text>
+          {{fakeText}}
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
   <v-row justify="center" align="center">
     <v-col md="8">
-      <v-card>
-        blah blah
+      <v-card shaped>
+        <v-card-text>
+          {{fakeText}}
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
 </v-container>
   
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      fakeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    }
+  }
+}
+</script>
