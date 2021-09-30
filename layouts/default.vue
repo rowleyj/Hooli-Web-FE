dfeaff82d41a81e0781f8a74122b382206efc674
Jesse Rowley <rowleyj@mcmaster.ca>
2021-09-29 20:12:04 -0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-btn fab class="mx-2" x-small>
+        <v-img src="/icon.png" style="border-radius: 50%" max-width="40" max-height="40"></v-img>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        color="primary"
+        outlined
+        class="mx-1"
+      >
+        Purchase
+      </v-btn>
+      <v-btn
+        color="primary"
+      >
+        Login
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      absolute
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hooli'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+<v-container>
+  <v-row justify="center" align="center">
+    <v-img src="/hooli.png" max-height="200" max-width="200"></v-img>
+  </v-row>
+  <v-row justify="center" align="center">
+    <h2>
+    Cycling made safe
+    </h2>
+  </v-row>
+  <v-row justify="center" align="center">
+    <v-col md="8">
+      <v-card>
+        blah blah
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row justify="center" align="center">
+    <v-col md="8">
+      <v-card>
+        blah blah
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row justify="center" align="center">
+    <v-col md="8">
+      <v-card>
+        blah blah
+      </v-card>
+    </v-col>
+  </v-row>
+</v-container>
+  
+</template>
